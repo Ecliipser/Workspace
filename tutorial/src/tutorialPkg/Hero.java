5d1291d7f69cef53d58704f744eb9b53fa42e568
MarcL4 <MarcL4@091DS-1182-22.student.cssd.ab.ca>
2018-11-22 12:11:32 -0700

    changes again

--- tutorial/src/tutorialPkg/Hero.java
+++ tutorial/src/tutorialPkg/Hero.java
@@ -5,22 +5,46 @@ public class Hero {
 	private String name;
 	private String power;
 	private int age;
+	private int health;
+	private int dmgPerHit;
+	
 	
 	public Hero() {
 		name = "Jane Doe";
 		power = "Super speed";
 		age = 20;
+		health = 100;
+		dmgPerHit = 10;
 	}
 //constructors
-	public Hero(String name, String power, int age) {
+	public Hero(String name, String power, int age, int health, int dmgPerHit) {
 		this.name = name;
 		this.power = power;
 		this.age = age;
+		this.health = health;
+		this.dmgPerHit = dmgPerHit;
 	}
 	
 	public void setPower(String power) {
 		this.power = power;
 	}
+	
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	
+	public int getDmgPerHit() {
+		return dmgPerHit;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void attack(Hero h) {
+		h.setHealth(h.getHealth() - dmgPerHit);
+	}
+	
 	public String toString() {
 		return "Name: " + name + " Power: " + power + " Age: " + age;
 	
--- tutorial/src/tutorialPkg/TestHero.java
+++ tutorial/src/tutorialPkg/TestHero.java
@@ -6,20 +6,32 @@ public class TestHero {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hero h1 = new Hero();
-		Hero h2 = new Hero("Superman", "Strength",35);
-		Hero h3 = new Hero("Spiderman","Webs",20);
-		Hero h4 = new Hero("Daneel","Dad",16);
+		Hero h2 = new Hero("Superman", "Strength",35,100,12);
+		Hero h3 = new Hero("Spiderman","Webs",20,125,20);
+		Hero h4 = new Hero("Daneel","Dad",16,9000,9000);
 		
 		
 		System.out.println(h1.toString());
 		System.out.println(h2.toString());
 		System.out.println(h3.toString());
+		System.out.println(h4.toString());
 		
 		Meteorite m = new Meteorite();
 		m.mutate(h1);
 		m.mutate(h2);
 		m.mutate(h3);
 		m.mutate(h4);
+		
+		System.out.println(h1.toString());
+		System.out.println(h2.toString());
+		System.out.println(h3.toString());
+		System.out.println(h4.toString());
+		System.out.println();
+		
+		while(h1.getHealth() > 0) {
+		h2.attack(h1);
+		System.out.println("Hero 1 health: " + h1.getHealth());
+	}//end while
 	}
 
 }
